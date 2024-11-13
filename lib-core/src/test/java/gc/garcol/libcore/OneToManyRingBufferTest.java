8a50636e0d3a8c29d5ed741dff3132141cfc5ed8
Thai Van <giacatluong20121996@gmail.com>
2024-11-13 23:33:22 +0700

    add/unit-test: consume message with limit

--- lib-core/src/test/java/gc/garcol/libcore/OneToManyRingBufferTest.java
+++ lib-core/src/test/java/gc/garcol/libcore/OneToManyRingBufferTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
 
 /**
  * @author thaivc
@@ -68,5 +71,59 @@ public class OneToManyRingBufferTest
         oneToManyRingBuffer.read(0, handler);
         oneToManyRingBuffer.read(1, handler);
         oneToManyRingBuffer.read(2, handler);
+    }
+
+    @Test
+    public void shouldConsumeWithLimitSuccessfully_1P3C_10()
+    {
+        List<String> messages = List.of(
+            "Hello, world! 1",
+            "Hello, world! 2",
+            "Hello, world! 3"
+        );
+
+        List<AtomicInteger> consumedIndexes = List.of(new AtomicInteger(), new AtomicInteger(), new AtomicInteger());
+
+        oneToManyRingBuffer = new OneToManyRingBuffer(10, 3);
+
+        for (int i = 0; i < messages.size(); i++)
+        {
+            ByteBufferUtil.put(messageBufferWriter, 0, messages.get(i).getBytes());
+            messageBufferWriter.flip();
+            oneToManyRingBuffer.write(i, messageBufferWriter);
+        }
+
+        Function<Integer, MessageHandler> handlerSupplier = (Integer consumerId) ->
+            (int msgTypeId, UnsafeBuffer buffer, int index, int length) -> {
+                System.out.println("----------");
+                System.out.println("consumerId: " + consumerId);
+                System.out.println("msgTypeId: " + msgTypeId);
+                System.out.println("index: " + index);
+                System.out.println("length: " + length);
+
+                messageBufferReader.clear();
+
+                buffer.getBytes(index, messageBufferReader, 0, length);
+                messageBufferReader.position(length);
+                messageBufferReader.flip();
+
+                byte[] messageBytes = new byte[length];
+                messageBufferReader.get(messageBytes);
+
+                System.out.println("message: " + new String(messageBytes));
+
+                Assertions.assertEquals(messages.get(msgTypeId), new String(messageBytes));
+
+                consumedIndexes.get(consumerId).incrementAndGet();
+                return true;
+            };
+
+        oneToManyRingBuffer.read(0, handlerSupplier.apply(0), 3);
+        oneToManyRingBuffer.read(1, handlerSupplier.apply(1), 2);
+        oneToManyRingBuffer.read(2, handlerSupplier.apply(2), 1);
+
+        Assertions.assertEquals(3, consumedIndexes.get(0).get());
+        Assertions.assertEquals(2, consumedIndexes.get(1).get());
+        Assertions.assertEquals(1, consumedIndexes.get(2).get());
     }
 }
